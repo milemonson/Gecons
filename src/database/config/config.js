6a7a936f75ef360e1c95da9dbf4923e872dcb633
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-11-10 18:44:08 -0300

    Agregado el puerto de la BD y configuraciones extra

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,20 +4,35 @@ module.exports = {
         "password": process.env.DEV_DB_PWD,
         "database": process.env.DEV_DB_NAME,
         "host": process.env.DEV_DB_HOST,
-        "dialect": "mysql"
+        "port" : process.env.DEV_DB_HOST,
+        "dialect": "mysql",
+        "define" : {
+            "underscored" : true,
+            "timestamps" : false
+        }
     },
     "test": {
         "username": process.env.TEST_DB_USR,
         "password": process.env.TEST_DB_PWD,
         "database": process.env.TEST_DB_NAME,
         "host": process.env.TEST_DB_HOST,
-        "dialect": "mysql"
+        "port" : process.env.TEST_DB_HOST,
+        "dialect": "mysql",
+        "define" : {
+            "underscored" : true,
+            "timestamps" : false
+        }
     },
     "production": {
         "username": process.env.PROD_DB_USR,
         "password": process.env.PROD_DB_PWD,
         "database": process.env.PROD_DB_NAME,
         "host": process.env.PROD_DB_HOST,
-        "dialect": "mysql"
+        "port" : process.env.PROD_DB_HOST,
+        "dialect": "mysql",
+        "define" : {
+            "underscored" : true,
+            "timestamps" : false
+        }
     }
 }
