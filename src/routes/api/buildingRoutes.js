b1a4e2390490365e136dd44f902529f44e74c5cc
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-11-17 14:48:05 -0300

    Borrados los endpoints que están demás

--- src/routes/api/buildingRoutes.js
+++ src/routes/api/buildingRoutes.js
@@ -6,12 +6,6 @@ router.get("/list", controller.listAll);
 
 router.get("/pages", controller.getPages);
 
-router.get("/:id", controller.detail);
-
-router.post("/create", controller.create);
-
-router.put("/edit/:id", controller.update);
-
 router.delete("/delete/:id", controller.delete);
 
 module.exports = router;
