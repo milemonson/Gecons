e9162741011aa7bd15d72dbcf73dd7bae619e2b6
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-11-17 16:35:25 -0300

    Agregadas las rutas básicas y un prototipo del controlador para las vistas de la entidad 'apartment'

---
+++ src/controllers/apartmentController.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+    /** Listado de todos los departamentos */
+    show : (req, res) => {
+
+    },
+
+    /** Detalle de un departamento */
+    detail : (req, res) => {
+        
+    },
+
+    /** Vista de creación */
+    create : (req, res) => {
+        
+    },
+
+    /** Procesamiento de la vista de creación */
+    store : (req, res) => {
+        
+    },
+
+    /** Vista de edición */
+    edit : (req, res) => {
+        
+    },
+
+    /** Procesamiento de la vista de edición */
+    update : (req, res) => {
+        
+    }
+
+}
