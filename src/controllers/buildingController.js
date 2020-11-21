4f62d614e0398acd5f022ff056d9d4781dbafd8b
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-11-21 14:33:10 -0300

    Procesada y validada la creación de edificios

--- src/controllers/buildingController.js
+++ src/controllers/buildingController.js
@@ -1,3 +1,9 @@
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const { Building } = require("../database/models");
+
+const NUMBER_OF_SALT = 12;
+
 module.exports = {
 
     /** Listado de todos los edificios registrados */
@@ -12,7 +18,33 @@ module.exports = {
 
     /** Procesamiento de la creación de un edificio */
     store : (req, res) => {
-        
+        let errors = validationResult(req);
+
+        if(errors.isEmpty()){
+            
+            let newBuilding = {
+                name : req.body.name,
+                password : bcrypt.hashSync(req.body.password, NUMBER_OF_SALT)
+            }
+
+            Building.create(newBuilding)
+                .then(created => {
+                    // TODO : Crear las carpetas pertinentes con mdirp
+                    // TODO : Procesar el mail
+                    res.redirect("/admin/buildings");
+                });
+                // TODO : Atajar el error
+
+        } else {
+
+            res.render("admin/addBuilding", {
+                errors : errors.mapped(),
+                userInput : {
+                    name : req.body.name,
+                    "send-to" : req.body["send-to"]
+                }
+            });
+        }
     },
 
     /** Edición de un edificio existente */
--- src/routes/buildingRoutes.js
+++ src/routes/buildingRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/buildingController");
+const createBuildingValidator = require("../validators/createBuildingValidator");
 
 // Listado
 router.get("/", controller.show);
 
 // Creación
 router.get("/add", controller.create);
-router.post("/add", controller.store);
+router.post("/add", createBuildingValidator, controller.store);
 
 // Edición
 router.get("/:id/edit", controller.edit);
