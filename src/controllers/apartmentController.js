faf779b248ef4a985474898d7cdf612eb4e00caf
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-11-24 15:35:50 -0300

    Creada  y ruteada unaversión base de la vista para la subida de deptos

--- src/controllers/apartmentController.js
+++ src/controllers/apartmentController.js
@@ -1,8 +1,10 @@
+const { Building, Apartment } = require("../database/models");
+
 module.exports = {
 
     /** Listado de todos los departamentos */
     show : (req, res) => {
-
+        res.render("admin/apartments");
     },
 
     /** Detalle de un departamento */
@@ -13,6 +15,13 @@ module.exports = {
     /** Vista de creación */
     create : (req, res) => {
         
+        Building.findByPk(Number(req.query.b), { attributes : ["id", "name"] })
+            .then(result => {
+                res.render("admin/addApartment", { 
+                    building : result
+                });
+            })
+
     },
 
     /** Procesamiento de la vista de creación */
@@ -22,7 +31,6 @@ module.exports = {
 
     /** Vista de edición */
     edit : (req, res) => {
-        
     },
 
     /** Procesamiento de la vista de edición */
