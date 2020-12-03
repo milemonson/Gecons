99dfe8e6d5ab9b9843be97b5eeabc49cd581d43a
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-12-03 14:45:45 -0300

    Añadida una ruta para la descarga de archivos a través de la API

--- src/routes/api/apartmentRoutes.js
+++ src/routes/api/apartmentRoutes.js
@@ -6,6 +6,8 @@ router.get("/list", controller.listAll);
 
 router.get("/pages", controller.getPages);
 
+router.get("/download/:id", controller.downloadDocument);
+
 router.delete("/delete/:id", controller.delete);
 
 module.exports = router;
